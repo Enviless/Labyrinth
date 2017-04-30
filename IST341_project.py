60d60b30c1a2f3559ad7ecaccdccb5466e4840ef
sqewad <lx19910131@gmail.com>
2017-05-01 07:52:31 +0900

    add hole edge collision

--- IST341_project.py
+++ IST341_project.py
@@ -1,4 +1,5 @@
 from visual import *
+import math
 
 scene = display(title='labyrinth')
 
@@ -6,21 +7,39 @@ r = 0.5
 l = 12.0
 
 ball = sphere(pos = vector(4 , 4 , r), radius = r, material = materials.rough, color = color.cyan)
-board = box(pos = (0,0,-0.5), size = (l,l, 1.0), material = materials.wood, color = color.orange)
+
+class Hole:
+    def __init__(self, pos, r):
+        self.pos = vector(pos[0], pos[1], 0)
+        self.r = r
+        self.graph = shapes.circle(pos=pos, radius=r)
+
+hole1 = Hole((5,-5),0.7)
+hole2 = Hole((-2,4.2), 0.7)
+holes = [hole1, hole2]
+
+rt = shapes.rectangle(pos=(0,0), width=l, height=l)
+straight = [(0,0,0),(0,0,-0.5)]
+sp = rt
+for hole in holes:
+    sp -= hole.graph
+board = extrusion(pos=straight, shape=sp, material = materials.wood, color = color.orange)
+
 wall1 = box(pos = (l/2.0, 0, r), axis = (0, 1.0, 0), size = (l+0.5, 0.5, 3.0*r), material = materials.wood, color = color.orange)
 wall2 = box(pos = (-l/2.0,0, r), axis = (0, 1.0, 0), size = (l+0.5, 0.5, 3.0*r), material = materials.wood, color = color.orange)
 wall3 = box(pos = (0, l/2.0, r), axis = (1.0, 0, 0), size = (l+0.5, 0.5, 3.0*r), material = materials.wood, color = color.orange)
 wall4 = box(pos = (0,-l/2.0, r), axis = (1.0, 0, 0), size = (l+0.5, 0.5, 3.0*r), material = materials.wood, color = color.orange)
 wall5 = box(pos = (1, 0, r), axis = (1.0, 1.0 ,0), size = (l/2.0, 0.5, 3.0*r), material = materials.wood, color = color.orange)
-cone1 = cone(pos=(-3,2,0.1), axis=(0,0,-1), radius = 0.7, color = color.black)
-#k=1 #magitude of velocity after collision / before collision
+walls = [wall1, wall2, wall3, wall4, wall5]
+
+
 def wall_collide( b, w ):
     """ w is a wall (a box with a length and width)
         b is a ball (an object with a pos)
         returns True if b collides with w (in length or width)
         returns False otherwise
     """
-    k = 0.8
+    k = 0.8 #k=1 #magitude of velocity after collision / before collision
     global velocity
     # w.pos is the center of the wall
     # b.pos is the position (center) of the ball
@@ -30,46 +49,68 @@ def wall_collide( b, w ):
               w.pos + norm(w.axis) * w.length/2.0 - norm(perpen) * w.height/2.0, 
               w.pos - norm(w.axis) * w.length/2.0 - norm(perpen) * w.height/2.0]
     for i in xrange(len(corner)):
-        corner[i].z = r
-    length_vector = proj(vector(b.pos.x, b.pos.y, r) - vector(w.pos.x, w.pos.y, r), w.axis)
-    width_vector = proj(vector(b.pos.x, b.pos.y, r) - vector(w.pos.x, w.pos.y, r), perpen)
+        corner[i].z = b.pos.z
+    length_vector = proj(b.pos - vector(w.pos.x, w.pos.y, b.pos.z), w.axis)
+    width_vector = proj(b.pos - vector(w.pos.x, w.pos.y, b.pos.z), perpen)
     lega = mag(length_vector)                              
     legb = mag(width_vector)            
-    if legb <= w.height/2.0 + r  and lega <= w.length/2.0: # w.height is along y axis, which is actually the width here
-        collide_point = vector(w.pos.x, w.pos.y, r) + length_vector + w.height/2 * norm(b.pos - vector(w.pos.x, w.pos.y, r) - length_vector)
+    if legb <= w.height/2.0 + b.radius  and lega <= w.length/2.0: # w.height is along y axis, which is actually the width here
+        collide_point = vector(w.pos.x, w.pos.y, b.pos.z) + length_vector + w.height/2 * norm(b.pos - vector(w.pos.x, w.pos.y, b.pos.z) - length_vector)
         velocity_parall = proj(velocity, w.axis)
         velocity_perpen = proj(velocity, perpen)
         #after collision
         velocity = velocity_parall - velocity_perpen * k
-        ball.pos = collide_point + r * norm(ball.pos - collide_point)
-    elif lega <= w.length/2.0 + r and legb <= w.height/2.0:
-        collide_point = vector(w.pos.x, w.pos.y, r) + width_vector + w.length/2 * norm(b.pos - vector(w.pos.x, w.pos.y, r) - width_vector)
+        b.pos = collide_point + b.radius * norm(b.pos - collide_point)
+    elif lega <= w.length/2.0 + b.radius and legb <= w.height/2.0:
+        collide_point = vector(w.pos.x, w.pos.y, b.pos.z) + width_vector + w.length/2 * norm(b.pos - vector(w.pos.x, w.pos.y, b.pos.z) - width_vector)
         velocity_parall = proj(velocity, perpen)
         velocity_perpen = proj(velocity, w.axis)
         #after collision
         velocity = velocity_parall - velocity_perpen * k
-        ball.pos = collide_point + r * norm(ball.pos - collide_point)
+        b.pos = collide_point + b.radius * norm(b.pos - collide_point)
     else:
         for i in xrange(4):
-            if mag(ball.pos - corner[i]) <= r:
-                perpen = ball.pos - corner[i]
+            if mag(b.pos - corner[i]) <= r:
+                perpen = b.pos - corner[i]
                 parall = rotate(perpen, radians(90), vector(0, 0, 1))
                 velocity_parall = proj(velocity, parall)
                 velocity_perpen = proj(velocity, perpen)
                 # after collision
                 velocity = velocity_parall - velocity_perpen * k
-                ball.pos = corner[i] + r * norm(ball.pos - corner[i])
+                b.pos = corner[i] + b.radius * norm(b.pos - corner[i])
+
+def hole_edge_colide(b, h):
+    k = 0.8
+    global velocity
+    proj_hole_to_ball_x_y = vector(b.pos.x, b.pos.y ,0) - h.pos
+    colide_point = h.pos + h.r * norm(proj_hole_to_ball_x_y)
+    colide_point_to_ball_center = b.pos - colide_point
+    hole_tangent = rotate(proj_hole_to_ball_x_y, radians(90), vector(0, 0, 1))
+    u = - norm(cross(colide_point_to_ball_center, hole_tangent))
+    v_xx = proj(velocity, u)
+    v_yy = proj(velocity, hole_tangent)
+    v_zz = proj(velocity, colide_point_to_ball_center)
+    if mag(proj_hole_to_ball_x_y) <= h.r:
+        if mag(colide_point - b.pos) <= b.radius:
+            v_zz = - v_zz
+            velocity = v_xx + v_yy + v_zz
+            b.pos = colide_point + b.radius * norm(colide_point_to_ball_center)
+        else:
+            pass
+            
+
+
 
 
 g = vector(0, 0, -9.8)
-RATE = 60
+RATE = 200
 dt = 1.0/(1.0*RATE)
 alpha = 0
 beta = 0
 velocity = vector(0, 0, 0)
 friction_coefficient = 0.03
 
-while True:
+while ball.pos.z >= (-5) * ball.radius:
     rate(RATE)
     # keyboard events
     if scene.kb.keys:
@@ -96,28 +137,31 @@ while True:
                 scene.forward = rotate(scene.forward, -twodeg, u)
                 beta -= 2
                 g = rotate(g, -twodeg, u)
-    if abs(velocity) > 0:
+    if mag(velocity) > 0:
         friction = -norm(velocity) * abs(g.z) * friction_coefficient
     else:
         if abs(vector(g.x, g.y, 0)) <= abs(g.z) * friction_coefficient:
             friction = -vector(g.x, g.y, 0)
         else:
             friction = -norm(vector(g.x, g.y, 0)) * abs(g.z) * friction_coefficient
-    dist_ball_hole = (ball.pos.x - cone1.pos.x)**2 + (ball.pos.y - cone1.pos.y)**2
-    a = vector(0, 0, 0)
-    if dist_ball_hole < 0.7**2:
-        a = 5 * norm(vector(cone1.pos.x, cone1.pos.y, 0) - vector(ball.pos.x, ball.pos.y, 0)) / dist_ball_hole
-    g_component = vector(g.x, g.y, 0)
-    accel = g_component + friction + a
+
+    for hole in holes:
+        proj_hole_to_ball_x_y = vector(ball.pos.x, ball.pos.y ,0) - hole.pos
+        if mag(proj_hole_to_ball_x_y) <= hole.r:
+            accel = g
+            break
+    else:
+        g_component = vector(g.x, g.y, 0)
+        accel = g_component + friction
     velocity += accel*dt
     ball.pos += velocity * dt
+    if ball.pos.z > ball.radius:
+        ball.pos.z = ball.radius
 
-    walls = [wall1, wall2, wall3, wall4, wall5]
     for wall in walls:
         wall_collide(ball, wall)
-    dist_ball_hole = (ball.pos.x - cone1.pos.x)**2 + (ball.pos.y - cone1.pos.y)**2
-    if dist_ball_hole < 0.2**2:
-        break
+    for hole in holes:
+        hole_edge_colide(ball, hole)
 
 
 
